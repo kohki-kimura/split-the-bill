9bc1eeb25a3821a6fd8ab4a5029ed66831c9afbc
kohki-kimura <kohki.soccer0217@gmail.com>
2020-10-16 16:26:24 +0900

    マイグレーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,20 @@
 ActiveRecord::Schema.define(version: 2020_10_16_060445) do
 
   create_table "caluclations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "total_payment", null: false
+    t.integer "total_people", null: false
+    t.string "group_name_one", null: false
+    t.integer "people_one", null: false
+    t.integer "ratio_one", null: false
+    t.integer "price_one", null: false
+    t.string "group_name_two", null: false
+    t.integer "people_two", null: false
+    t.integer "ratio_two", null: false
+    t.integer "price_two", null: false
+    t.string "group_name_three", null: false
+    t.integer "people_three", null: false
+    t.integer "ratio_three", null: false
+    t.integer "price_three", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
