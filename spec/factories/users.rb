1bf30f6f80198a57091403a1604d50a3b29ee4f3
kohki-kimura <kohki.soccer0217@gmail.com>
2020-10-16 13:35:03 +0900

    単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {"test1234"}
+    password_confirmation {"test1234"}
+  end
+end
