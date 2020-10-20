16a7634a0aa3c7f370ebc57eeeab773e872ec8d4
kohki-kimura <kohki.soccer0217@gmail.com>
2020-10-20 15:10:11 +0900

    バリデーションの修正

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -1,9 +1,12 @@
 class Calculation < ApplicationRecord
   belongs_to :user
 
-  with_options presence: true do
+  with_options numericality: {greater_than_or_equal_to: 0} do
     validates :total_payment
     validates :total_people
+    validates :people_one
+    validates :ratio_one
+    validates :price_one
     validates :people_two
     validates :ratio_two
     validates :price_two
